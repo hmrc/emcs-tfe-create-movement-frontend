8f4aefe2dc53c1d2aa3df96d2f52a7a658b16526
Graham Cook <grahampaulcook@users.noreply.github.com>
2023-09-28 17:42:03 +0100

    ETFE-2078 Adding CAM-TA01 (#40)

---
+++ test/views/sections/transportArranger/TransportArrangerViewSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.sections.transportArranger
+
+import base.ViewSpecBase
+import fixtures.messages.sections.transportArranger.TransportArrangerMessages
+import forms.sections.transportArranger.TransportArrangerFormProvider
+import models.NormalMode
+import models.requests.DataRequest
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.i18n.Messages
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import views.html.sections.transportArranger.TransportArrangerView
+import views.{BaseSelectors, ViewBehaviours}
+
+class TransportArrangerViewSpec extends ViewSpecBase with ViewBehaviours {
+  object Selectors extends BaseSelectors
+
+  "TransportArrangerView" - {
+
+    Seq(TransportArrangerMessages.English, TransportArrangerMessages.Welsh).foreach { messagesForLanguage =>
+
+      s"when being rendered in lang code of '${messagesForLanguage.lang.code}'" - {
+
+        implicit val msgs: Messages = messages(app, messagesForLanguage.lang)
+        implicit val request: DataRequest[AnyContentAsEmpty.type] = dataRequest(FakeRequest(), emptyUserAnswers)
+
+        val view = app.injector.instanceOf[TransportArrangerView]
+        val form = app.injector.instanceOf[TransportArrangerFormProvider].apply()
+
+        implicit val doc: Document = Jsoup.parse(
+          view(
+            form = form,
+            mode = NormalMode
+          ).toString())
+
+        behave like pageWithExpectedElementsAndMessages(Seq(
+          Selectors.title -> messagesForLanguage.title,
+          Selectors.h1 -> messagesForLanguage.heading,
+          Selectors.radioButton(1) -> messagesForLanguage.consignorRadioOption,
+          Selectors.radioButton(2) -> messagesForLanguage.consigneeRadioOption,
+          Selectors.radioButton(3) -> messagesForLanguage.goodsOwnerRadioOption,
+          Selectors.radioButton(4) -> messagesForLanguage.otherRadioOption,
+          Selectors.button -> messagesForLanguage.saveAndContinue,
+          Selectors.link(1) -> messagesForLanguage.savePreviousAnswersAndExit
+        ))
+      }
+    }
+  }
+}
+

871ad5dcf660bbfd595a3a282661eb445aa1afef
grahampaulcook <grahampaulcook@users.noreply.github.com>
2023-09-29 06:44:22 +0100

    Move scaffold migrated code to sections

---
+++ move2section.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# first argument is the section name i.e journeyType / transportArranger
+SECTION=${1:?Usage: $0 <<SECTION NAME GOES HERE>>}
+
+# list of all untracked git files (after you've just run ./migrate.sh)
+FILES="$(git ls-files --others --exclude-standard)"
+
+# for each untracked git file
+for f in $FILES
+do
+	# get the path and file separately
+	DIR="$(dirname "${f}")"
+	FILE="$(basename "${f}")"
+
+  # Create all intermediate directories (-p)
+  mkdir -p $DIR/sections/$SECTION
+
+  echo "moving file $DIR/$FILE into section $DIR/sections/$SECTION/$FILE"
+	mv -f $DIR/$FILE $DIR/sections/$SECTION/$FILE
+done

270f2f874b99f049fd2924a06cc650f0f9201c35
james-work-account <31282758+james-work-account@users.noreply.github.com>
2023-10-11 13:39:47 +0100

    [ETFE-2213] add destination type page to LRN submit screen

--- app/controllers/sections/info/LocalReferenceNumberController.scala
+++ app/controllers/sections/info/LocalReferenceNumberController.scala
@@ -16,16 +16,18 @@
 
 package controllers.sections.info
 
-import config.SessionKeys.DEFERRED_MOVEMENT
+import config.SessionKeys.{DEFERRED_MOVEMENT, DESTINATION_TYPE}
 import controllers.BaseNavigationController
 import controllers.actions._
 import forms.LocalReferenceNumberFormProvider
 import models.requests.UserRequest
+import models.sections.info.movementScenario.MovementScenario
 import models.{NormalMode, UserAnswers}
 import navigation.Navigator
-import pages.sections.info.{DeferredMovementPage, LocalReferenceNumberPage}
+import pages.sections.info.{DeferredMovementPage, DestinationTypePage, LocalReferenceNumberPage}
 import play.api.data.Form
 import play.api.i18n.MessagesApi
+import play.api.libs.json.JsString
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 import services.UserAnswersService
 import views.html.sections.info.LocalReferenceNumberView
@@ -47,27 +49,29 @@ class LocalReferenceNumberController @Inject()(
 
   def onPageLoad(ern: String): Action[AnyContent] =
     (auth(ern) andThen userAllowed).async { implicit request =>
-      withDeferredMovementAnswer { isDeferred =>
-        renderView(Ok, formProvider(isDeferred), isDeferred)
+      withGuard {
+        case (_, isDeferred) =>
+          renderView(Ok, formProvider(isDeferred), isDeferred)
       }
     }
 
   def onSubmit(ern: String): Action[AnyContent] =
     (auth(ern) andThen userAllowed).async { implicit request =>
-      withDeferredMovementAnswer { isDeferred =>
-        formProvider(isDeferred).bindFromRequest().fold(
-          renderView(BadRequest, _, isDeferred),
-          lrn =>
-            userAnswersService.get(ern, lrn).flatMap {
-              case Some(_) =>
-                //TODO: Redirect to LRN already exists page when designed and built
-                logger.debug("[onSubmit] Draft already exists with this LRN")
-                Future.successful(Redirect(testOnly.controllers.routes.UnderConstructionController.onPageLoad()))
-              case _ =>
-                logger.debug("[onSubmit] No Draft exists with this LRN, creating a new draft entry in the UserAnswers repo")
-                createDraftEntryAndRedirect(lrn)
-            }
-        )
+      withGuard {
+        case (_, isDeferred) =>
+          formProvider(isDeferred).bindFromRequest().fold(
+            renderView(BadRequest, _, isDeferred),
+            lrn =>
+              userAnswersService.get(ern, lrn).flatMap {
+                case Some(_) =>
+                  //TODO: Redirect to LRN already exists page when designed and built
+                  logger.debug("[onSubmit] Draft already exists with this LRN")
+                  Future.successful(Redirect(testOnly.controllers.routes.UnderConstructionController.onPageLoad()))
+                case _ =>
+                  logger.debug("[onSubmit] No Draft exists with this LRN, creating a new draft entry in the UserAnswers repo")
+                  createDraftEntryAndRedirect(lrn)
+              }
+          )
       }
     }
 
@@ -81,17 +85,43 @@ class LocalReferenceNumberController @Inject()(
         f(isDeferred.toBoolean)
       case _ =>
         logger.warn(s"[withIsDeferredMovementAnswer] No answer for Deferred Movement question, redirecting to get answer")
-        //TODO: Redirect back to the Deferred Movement question to capture answer
-        Future.successful(Redirect(testOnly.controllers.routes.UnderConstructionController.onPageLoad()))
+        Future.successful(Redirect(controllers.sections.info.routes.DeferredMovementController.onPageLoad(request.ern)))
+    }
+
+  private def withDestinationTypePageAnswer(f: MovementScenario => Future[Result])(implicit request: UserRequest[_]): Future[Result] =
+    request.session.get(DESTINATION_TYPE) match {
+      case Some(destinationTypeAnswer) =>
+        logger.debug(s"[withDestinationTypeAnswer] Destination Type answer: '$destinationTypeAnswer'")
+
+        JsString(destinationTypeAnswer).asOpt[MovementScenario] match {
+          case Some(movementScenario) => f(movementScenario)
+          case _ =>
+            logger.warn(s"[withDestinationTypeAnswer] Unable to parse answer for Destination Type question, redirecting to get answer")
+            Future.successful(Redirect(controllers.sections.info.routes.DestinationTypeController.onPageLoad(request.ern)))
+        }
+      case _ =>
+        logger.warn(s"[withDestinationTypeAnswer] No answer for Destination Type question, redirecting to get answer")
+        Future.successful(Redirect(controllers.sections.info.routes.DestinationTypeController.onPageLoad(request.ern)))
     }
 
   private def createDraftEntryAndRedirect(lrn: String)(implicit request: UserRequest[_]): Future[Result] =
-    withDeferredMovementAnswer { isDeferred =>
-      val userAnswers = UserAnswers(request.ern, lrn)
-        .set(DeferredMovementPage, isDeferred)
-        .set(LocalReferenceNumberPage, lrn)
-      userAnswersService.set(userAnswers).map { answers =>
-        Redirect(navigator.nextPage(LocalReferenceNumberPage, NormalMode, answers))
+    withGuard {
+      case (destinationTypePageAnswer, isDeferred) =>
+        val userAnswers = UserAnswers(request.ern, lrn)
+          .set(DestinationTypePage, destinationTypePageAnswer)
+          .set(DeferredMovementPage, isDeferred)
+          .set(LocalReferenceNumberPage, lrn)
+
+        userAnswersService.set(userAnswers).map { answers =>
+          Redirect(navigator.nextPage(LocalReferenceNumberPage, NormalMode, answers))
+        }
+    }
+
+  private def withGuard(f: (MovementScenario, Boolean) => Future[Result])(implicit request: UserRequest[_]): Future[Result] = {
+    withDestinationTypePageAnswer { destinationTypePageAnswer =>
+      withDeferredMovementAnswer { isDeferred =>
+        f(destinationTypePageAnswer, isDeferred)
       }
     }
+  }
 }

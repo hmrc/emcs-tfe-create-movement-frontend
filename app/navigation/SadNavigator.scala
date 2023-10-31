7f6ad6235bc8636edc7a963b106dad26cbe19c1a
james-work-account <31282758+james-work-account@users.noreply.github.com>
2023-10-31 14:23:53 +0000

    [ETFE-2380] added :(

---
+++ app/navigation/SadNavigator.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import controllers.routes
+import models.{CheckMode, Mode, NormalMode, ReviewMode, UserAnswers}
+import pages.Page
+import play.api.mvc.Call
+
+import javax.inject.Inject
+
+class SadNavigator @Inject() extends BaseNavigator {
+
+  private val normalRoutes: Page => UserAnswers => Call = {
+    case _ =>
+      (_: UserAnswers) => testOnly.controllers.routes.UnderConstructionController.onPageLoad()
+  }
+
+  private[navigation] val checkRouteMap: Page => UserAnswers => Call = {
+    case _ =>
+      // TODO: update to Sad add-to-list when built
+      (_: UserAnswers) => testOnly.controllers.routes.UnderConstructionController.onPageLoad()
+  }
+
+  private[navigation] val reviewRouteMap: Page => UserAnswers => Call = {
+    case _ =>
+      (userAnswers: UserAnswers) => routes.CheckYourAnswersController.onPageLoad(userAnswers.ern, userAnswers.draftId)
+  }
+
+  override def nextPage(page: Page, mode: Mode, userAnswers: UserAnswers): Call = mode match {
+    case NormalMode =>
+      normalRoutes(page)(userAnswers)
+    case CheckMode =>
+      checkRouteMap(page)(userAnswers)
+    case ReviewMode =>
+      reviewRouteMap(page)(userAnswers)
+  }
+}
---
+++ test/controllers/sections/sad/SadIndexControllerSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.sections.sad
+
+import base.SpecBase
+import play.api.http.Status.SEE_OTHER
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class SadIndexControllerSpec extends SpecBase {
+  "SadIndexController" - {
+
+    "when SadSection.isCompleted" - {
+      // TODO: remove ignore when CYA page is built
+      "must redirect to the CYA controller" ignore {
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+        running(application) {
+
+          val request = FakeRequest(GET, controllers.sections.sad.routes.SadIndexController.onPageLoad(testErn, testDraftId).url)
+          val result = route(application, request).value
+
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result) mustBe Some(testOnly.controllers.routes.UnderConstructionController.onPageLoad().url)
+        }
+      }
+    }
+
+    "must redirect to the sad import number controller" in {
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+
+        val request = FakeRequest(GET, controllers.sections.sad.routes.SadIndexController.onPageLoad(testErn, testDraftId).url)
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result) mustBe Some(testOnly.controllers.routes.UnderConstructionController.onPageLoad().url)
+      }
+    }
+  }
+}

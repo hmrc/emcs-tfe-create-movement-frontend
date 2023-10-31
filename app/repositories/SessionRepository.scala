c8c63e0cc8c360d16cdfcd029e7ec3b560c28eea
james-work-account <31282758+james-work-account@users.noreply.github.com>
2023-10-31 14:36:45 +0000

    [ETFE-fix] update imports on session repository

--- app/repositories/SessionRepository.scala
+++ app/repositories/SessionRepository.scala
@@ -27,7 +27,7 @@ import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 
 import java.time.Instant
 import java.util.concurrent.TimeUnit
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 

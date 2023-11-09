a910ad6c5f2b8d47602329b9b30019b22e1fcb43
Nick Gravgaard <148371638+nickg-hmrc@users.noreply.github.com>
2023-11-09 18:29:06 +0000

    Remove optional ERN parameter from emcsTfeHomeUrl

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -60,13 +60,12 @@ class AppConfig @Inject()(servicesConfig: ServicesConfig, configuration: Configu
   private lazy val feedbackFrontendHost: String = configuration.get[String]("feedback-frontend.host")
   lazy val feedbackFrontendSurveyUrl: String = s"$feedbackFrontendHost/feedback/$deskproName/beta"
 
-  def emcsTfeHomeUrl(ern: Option[String] = None): String = {
+  def emcsTfeHomeUrl: String =
     if (isEnabled(ReturnToLegacy)) {
-      configuration.get[String]("urls.legacy.atAGlance") + ern.fold("")(s"/" + _)
+      configuration.get[String]("urls.legacy.atAGlance")
     } else {
       configuration.get[String]("urls.emcsTfeHome")
     }
-  }
 
   def returnToDraft(implicit request: DataRequest[_]): String = controllers.routes.DraftMovementController.onPageLoad(request.ern, request.draftId).url
 
--- test/config/AppConfigSpec.scala
+++ test/config/AppConfigSpec.scala
@@ -69,27 +69,16 @@ class AppConfigSpec extends SpecBase with BeforeAndAfterEach with FeatureSwitchi
 
     ".emcsTfeHomeUrl" - {
       "should generate the correct url" - {
-        s"when the $ReturnToLegacy feature switch is enabled with ERN" in {
-          enable(ReturnToLegacy)
-          val ern = "123"
-
-          appConfig.emcsTfeHomeUrl(Some(ern)) mustBe s"http://localhost:8080/emcs/trader/$ern"
-        }
-        s"when the $ReturnToLegacy feature switch is enabled with no ERN" in {
+        s"when the $ReturnToLegacy feature switch is enabled" in {
           enable(ReturnToLegacy)
 
-          appConfig.emcsTfeHomeUrl() mustBe "http://localhost:8080/emcs/trader"
-        }
-        s"when the $ReturnToLegacy feature switch is disabled with ERN" in {
-          disable(ReturnToLegacy)
-
-          appConfig.emcsTfeHomeUrl(Some("123")) mustBe "http://localhost:8310/emcs-tfe"
+          appConfig.emcsTfeHomeUrl mustBe "http://localhost:8080/emcs/trader"
         }
 
-        s"when the $ReturnToLegacy feature switch is disabled with no ERN" in {
+        s"when the $ReturnToLegacy feature switch is disabled" in {
           disable(ReturnToLegacy)
 
-          appConfig.emcsTfeHomeUrl() mustBe "http://localhost:8310/emcs-tfe"
+          appConfig.emcsTfeHomeUrl mustBe "http://localhost:8310/emcs-tfe"
         }
       }
     }

3fa397b49b8102a46f778189d77a8fbe08f3c3d2
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2024-01-29 15:36:03 +0000

    ETFE-3221 add incomplete tag to incomplete transport unit section & update tests

--- app/viewmodels/helpers/TransportUnitsAddToListHelper.scala
+++ app/viewmodels/helpers/TransportUnitsAddToListHelper.scala
@@ -20,17 +20,21 @@ import controllers.sections.transportUnit.{routes => transportUnitRoutes}
 import models.Index
 import models.requests.DataRequest
 import models.sections.transportUnit.TransportUnitType.FixedTransport
-import pages.sections.transportUnit.TransportUnitTypePage
+import pages.sections.transportUnit.{TransportUnitSection, TransportUnitTypePage}
 import play.api.i18n.Messages
+import play.twirl.api.HtmlFormat
 import queries.TransportUnitsCount
 import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Actions, Card, CardTitle, SummaryList}
 import viewmodels.checkAnswers.sections.transportUnit._
 import viewmodels.govuk.summarylist._
+import viewmodels.taskList.Completed
+import views.html.components.{link, tag, span}
 
 import javax.inject.Inject
 
-class TransportUnitsAddToListHelper @Inject()(implicit link: views.html.components.link) {
+class TransportUnitsAddToListHelper @Inject()(implicit link: link, tag: tag, span: span) {
 
   def allTransportUnitsSummary()(implicit request: DataRequest[_], messages: Messages): Seq[SummaryList] = {
     request.userAnswers.get(TransportUnitsCount) match {
@@ -41,25 +45,50 @@ class TransportUnitsAddToListHelper @Inject()(implicit link: views.html.componen
 
   private def summaryList(idx: Index)(implicit request: DataRequest[_], messages: Messages): SummaryList = {
     val isTransportUnitAFixedTransportInstallation = request.userAnswers.get(TransportUnitTypePage(idx)).contains(FixedTransport)
+
     SummaryListViewModel(
       rows = Seq(
         Some(TransportUnitTypeSummary.row(idx)),
-        if(!isTransportUnitAFixedTransportInstallation) Some(TransportUnitIdentitySummary.row(idx)) else None,
-        if(!isTransportUnitAFixedTransportInstallation) Some(TransportSealChoiceSummary.row(idx)) else None,
-        if(!isTransportUnitAFixedTransportInstallation) Some(TransportSealTypeSummary.row(idx)) else None,
-        if(!isTransportUnitAFixedTransportInstallation) Some(TransportSealInformationSummary.row(idx)) else None,
-        if(!isTransportUnitAFixedTransportInstallation) Some(TransportUnitGiveMoreInformationSummary.row(idx)) else None
+        if (!isTransportUnitAFixedTransportInstallation) Some(TransportUnitIdentitySummary.row(idx)) else None,
+        if (!isTransportUnitAFixedTransportInstallation) Some(TransportSealChoiceSummary.row(idx)) else None,
+        if (!isTransportUnitAFixedTransportInstallation) Some(TransportSealTypeSummary.row(idx)) else None,
+        if (!isTransportUnitAFixedTransportInstallation) Some(TransportSealInformationSummary.row(idx)) else None,
+        if (!isTransportUnitAFixedTransportInstallation) Some(TransportUnitGiveMoreInformationSummary.row(idx)) else None
       ).flatMap(_.flatten)
-    ).copy(card = Some(Card(
-      title = Some(CardTitle(Text(messages("transportUnitsAddToList.transportUnitCardTitle", idx.displayIndex)))),
-      actions = Some(Actions(items = Seq(
-        ActionItemViewModel(
-          content = Text(messages("site.remove")),
-          href = transportUnitRoutes.TransportUnitRemoveUnitController.onPageLoad(request.userAnswers.ern, request.userAnswers.draftId, idx).url,
-          id = s"removeTransportUnit${idx.displayIndex}"
-        ).withVisuallyHiddenText(messages("transportUnitsAddToList.transportUnitCardTitle", idx.displayIndex))
-      ))))
+    ).copy(card = Some(
+      Card(
+        title = Some(createCardTitle(idx)),
+        actions = Some(
+          Actions(
+            items = Seq(
+              ActionItemViewModel(
+                content = Text(messages("site.remove")),
+                href = transportUnitRoutes.TransportUnitRemoveUnitController.onPageLoad(request.userAnswers.ern, request.userAnswers.draftId, idx).url,
+                id = s"removeTransportUnit${idx.displayIndex}"
+              ).withVisuallyHiddenText(messages("transportUnitsAddToList.transportUnitCardTitle", idx.displayIndex))
+            )
+          )
+        )
+      )
     ))
+  }
+
+  private def createCardTitle(idx: Index)(implicit request: DataRequest[_], messages: Messages): CardTitle = {
+    if (TransportUnitSection(idx).status == Completed) {
+      CardTitle(Text(messages("transportUnitsAddToList.transportUnitCardTitle", idx.displayIndex)))
+    } else {
+      CardTitle(
+        HtmlContent(HtmlFormat.fill(
+          Seq(
+            span(messages("transportUnitsAddToList.transportUnitCardTitle", idx.displayIndex), Some("govuk-!-margin-right-2")),
+            tag(
+              message = messages("taskListStatus.incomplete"),
+              colour = "red"
+            )
+          )
+        ))
+      )
+    }
   }
 
 }

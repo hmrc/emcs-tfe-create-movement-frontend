ed3a3b844ec87f74c3508c92d36ab381355a636a
Joe Jones <33491932+joenjones07@users.noreply.github.com>
2024-05-03 15:12:33 +0100

    ETFE-3766: RIM validation for Consignee Address page (#300)

--- app/forms/AddressFormProvider.scala
+++ app/forms/AddressFormProvider.scala
@@ -21,6 +21,7 @@ import config.Constants.XI_POSTCODE
 import forms.mappings.Mappings
 import models.UserAddress
 import models.requests.DataRequest
+import pages.sections.consignee.{ConsigneeAddressPage, ConsigneeExcisePage}
 import pages.sections.consignor.ConsignorAddressPage
 import pages.sections.dispatch.{DispatchAddressPage, DispatchWarehouseExcisePage}
 import pages.{Page, QuestionPage}
@@ -41,22 +42,22 @@ class AddressFormProvider @Inject() extends Mappings {
     Form(mapping(
       "property" -> optional(text()
         .verifying(maxLength(propertyMax, "address.property.error.length"))
-        .verifying(regexp(ALPHANUMERIC_REGEX, "address.property.error.characters"))
+        .verifying(regexp(ALPHANUMERIC_REGEX, "address.property.error.character"))
         .verifying(regexpUnlessEmpty(XSS_REGEX, "address.property.error.invalid"))),
 
       "street" -> text("address.street.error.required")
         .verifying(maxLength(streetMax, "address.street.error.length"))
-        .verifying(regexp(ALPHANUMERIC_REGEX, "address.street.error.characters"))
+        .verifying(regexp(ALPHANUMERIC_REGEX, "address.street.error.character"))
         .verifying(regexpUnlessEmpty(XSS_REGEX, "address.street.error.invalid")),
 
       "town" -> text("address.town.error.required")
         .verifying(maxLength(townMax, "address.town.error.length"))
-        .verifying(regexp(ALPHANUMERIC_REGEX, "address.town.error.characters"))
+        .verifying(regexp(ALPHANUMERIC_REGEX, "address.town.error.character"))
         .verifying(regexpUnlessEmpty(XSS_REGEX, "address.town.error.invalid")),
 
       "postcode" -> text("address.postcode.error.required")
         .verifying(maxLength(postcodeMax, "address.postcode.error.length"))
-        .verifying(regexp(ALPHANUMERIC_REGEX, "address.postcode.error.characters"))
+        .verifying(regexp(ALPHANUMERIC_REGEX, "address.postcode.error.character"))
         .verifying(regexpUnlessEmpty(XSS_REGEX, "address.postcode.error.invalid"))
         .verifying(getExtraPostcodeValidationForPage(page): _*)
     )(UserAddress.apply)(UserAddress.unapply)
@@ -64,15 +65,17 @@ class AddressFormProvider @Inject() extends Mappings {
 
   private def getExtraPostcodeValidationForPage(page: Page)(implicit request: DataRequest[_]): Seq[Constraint[String]] = {
 
-    def niPostcode(isNi: Boolean): Seq[Constraint[String]] =
-      if(isNi) Seq(startsWith(XI_POSTCODE, "address.postcode.error.mustStartWithBT")) else {
-        Seq(doesNotStartWith(XI_POSTCODE, "address.postcode.error.mustNotStartWithBT"))
+    def niPostcode(isNi: Boolean, page: Page): Seq[Constraint[String]] =
+      if(isNi) Seq(startsWith(XI_POSTCODE, s"address.postcode.error.$page.mustStartWithBT")) else {
+        Seq(doesNotStartWith(XI_POSTCODE, s"address.postcode.error.$page.mustNotStartWithBT"))
       }
 
     page match {
-      case ConsignorAddressPage => niPostcode(request.isNorthernIrelandErn)
+      case ConsignorAddressPage => niPostcode(request.isNorthernIrelandErn, page)
+      case ConsigneeAddressPage if request.userAnswers.get(ConsigneeExcisePage).nonEmpty =>
+        niPostcode(request.userAnswers.get(ConsigneeExcisePage).exists(_.startsWith(Constants.NI_PREFIX)), page)
       case DispatchAddressPage if request.userAnswers.get(DispatchWarehouseExcisePage).nonEmpty =>
-        niPostcode(request.userAnswers.get(DispatchWarehouseExcisePage).exists(_.startsWith(Constants.NI_PREFIX)))
+        niPostcode(request.userAnswers.get(DispatchWarehouseExcisePage).exists(_.startsWith(Constants.NI_PREFIX)), page)
       case _ => Seq.empty
     }
   }
--- test/controllers/sections/consignee/ConsigneeAddressControllerSpec.scala
+++ test/controllers/sections/consignee/ConsigneeAddressControllerSpec.scala
@@ -26,7 +26,7 @@ import models.{NormalMode, UserAddress, UserAnswers}
 import navigation.FakeNavigators.FakeConsigneeNavigator
 import pages.sections.consignee.ConsigneeAddressPage
 import play.api.data.Form
-import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded, Call}
+import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import views.html.AddressView
@@ -34,20 +34,18 @@ import views.html.AddressView
 import scala.concurrent.Future
 
 class ConsigneeAddressControllerSpec extends SpecBase with MockUserAnswersService with UserAddressFixtures {
-
   lazy val formProvider: AddressFormProvider = new AddressFormProvider()
-  lazy val form: Form[UserAddress] = formProvider(ConsigneeAddressPage)(dataRequest(FakeRequest(), emptyUserAnswers))
+  lazy val form: Form[UserAddress] = formProvider(ConsigneeAddressPage)(dataRequest(FakeRequest(), emptyUserAnswers, ern = testGreatBritainErn))
   lazy val view: AddressView = app.injector.instanceOf[AddressView]
 
   lazy val consigneeAddressRoute: String =
-    controllers.sections.consignee.routes.ConsigneeAddressController.onPageLoad(testErn, testLrn, NormalMode).url
-  lazy val consigneeAddressOnSubmit: Call =
-    controllers.sections.consignee.routes.ConsigneeAddressController.onSubmit(testErn, testDraftId, NormalMode)
+    controllers.sections.consignee.routes.ConsigneeAddressController.onPageLoad(testGreatBritainErn, testDraftId, NormalMode).url
 
+  def consigneeAddressOnSubmit(ern: String = testGreatBritainErn): Call =
+    controllers.sections.consignee.routes.ConsigneeAddressController.onSubmit(ern, testDraftId, NormalMode)
 
   class Fixture(optUserAnswers: Option[UserAnswers] = Some(emptyUserAnswers)) {
-
-    val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(GET, consigneeAddressRoute)
+    val request = FakeRequest(GET, consigneeAddressRoute)
 
     lazy val testController = new ConsigneeAddressController(
       messagesApi,
@@ -57,7 +55,7 @@ class ConsigneeAddressControllerSpec extends SpecBase with MockUserAnswersServic
       new FakeDataRetrievalAction(optUserAnswers, Some(testMinTraderKnownFacts)),
       dataRequiredAction,
       fakeBetaAllowListAction,
-      new AddressFormProvider(),
+      formProvider,
       messagesControllerComponents,
       view
     )
@@ -65,40 +63,42 @@ class ConsigneeAddressControllerSpec extends SpecBase with MockUserAnswersServic
 
   "ConsigneeAddress Controller" - {
     "must return OK and the correct view for a GET" in new Fixture() {
-      val result = testController.onPageLoad(testErn, testDraftId, NormalMode)(request)
+      val result = testController.onPageLoad(testGreatBritainErn, testDraftId, NormalMode)(request)
 
       status(result) mustEqual OK
       contentAsString(result) mustEqual view(
         form = form,
         addressPage = ConsigneeAddressPage,
-        call = consigneeAddressOnSubmit
-      )(dataRequest(request), messages(request)).toString
+        call = consigneeAddressOnSubmit()
+      )(dataRequest(request, ern = testGreatBritainErn), messages(request)).toString
     }
 
-    "must populate the view correctly on a GET when the question has previously been answered" in new Fixture(
-      Some(emptyUserAnswers.set(ConsigneeAddressPage, userAddressModelMax))) {
-
-      val result = testController.onPageLoad(testErn, testDraftId, NormalMode)(request)
+    "must populate the view correctly on a GET when the question has previously been answered" in new Fixture(Some(emptyUserAnswers
+      .set(ConsigneeAddressPage, userAddressModelMax)
+    )) {
+      val result = testController.onPageLoad(testGreatBritainErn, testDraftId, NormalMode)(request)
 
       status(result) mustEqual OK
       contentAsString(result) mustEqual view(
         form = form.fill(userAddressModelMax),
         addressPage = ConsigneeAddressPage,
-        call = consigneeAddressOnSubmit
-      )(dataRequest(request), messages(request)).toString
+        call = consigneeAddressOnSubmit()
+      )(dataRequest(request, ern = testGreatBritainErn), messages(request)).toString
     }
 
     "must redirect to the next page when valid data is submitted" in new Fixture() {
+
       MockUserAnswersService.set().returns(Future.successful(emptyUserAnswers))
+      val req =
+        FakeRequest(POST, consigneeAddressRoute)
+          .withFormUrlEncodedBody(
+            ("property", userAddressModelMax.property.value),
+            ("street", userAddressModelMax.street),
+            ("town", userAddressModelMax.town),
+            ("postcode", userAddressModelMax.postcode)
+          )
 
-      val req: FakeRequest[AnyContentAsFormUrlEncoded] = FakeRequest(POST, consigneeAddressOnSubmit.url).withFormUrlEncodedBody(
-        ("property", userAddressModelMax.property.value),
-        ("street", userAddressModelMax.street),
-        ("town", userAddressModelMax.town),
-        ("postcode", userAddressModelMax.postcode)
-      )
-
-      val result = testController.onSubmit(testErn, testDraftId, NormalMode)(req)
+      val result = testController.onSubmit(testGreatBritainErn, testDraftId, NormalMode)(req)
 
       status(result) mustEqual SEE_OTHER
       redirectLocation(result).value mustEqual testOnwardRoute.url
@@ -109,27 +109,57 @@ class ConsigneeAddressControllerSpec extends SpecBase with MockUserAnswersServic
 
       val boundForm = form.bind(Map("value" -> ""))
 
+      val result = testController.onSubmit(testGreatBritainErn, testDraftId, NormalMode)(req)
+
+      status(result) mustEqual BAD_REQUEST
+      contentAsString(result) mustEqual view(
+        form = boundForm,
+        addressPage = ConsigneeAddressPage,
+        call = consigneeAddressOnSubmit()
+      )(dataRequest(req, ern = testGreatBritainErn), messages(req)).toString
+    }
+
+    "must return a Bad Request and errors when a 'BT' postcode is entered for a GB trader" in new Fixture() {
+      val req = FakeRequest(POST, consigneeAddressRoute).withFormUrlEncodedBody(("value", "BT11AA"))
+
+      val boundForm = form.bind(Map("value" -> "BT11AA"))
+
+      val result = testController.onSubmit(testGreatBritainErn, testDraftId, NormalMode)(req)
+
+      status(result) mustEqual BAD_REQUEST
+      contentAsString(result) mustEqual view(
+        form = boundForm,
+        addressPage = ConsigneeAddressPage,
+        call = consigneeAddressOnSubmit()
+      )(dataRequest(req, ern = testGreatBritainErn), messages(req)).toString
+    }
+
+    "must return a Bad Request and errors when a GB postcode is entered for an XI trader" in new Fixture() {
+      val req = FakeRequest(POST, consigneeAddressRoute).withFormUrlEncodedBody(("value", "B11AA"))
+
+      val boundForm = form.bind(Map("value" -> "B11AA"))
+
       val result = testController.onSubmit(testErn, testDraftId, NormalMode)(req)
 
       status(result) mustEqual BAD_REQUEST
       contentAsString(result) mustEqual view(
         form = boundForm,
         addressPage = ConsigneeAddressPage,
-        call = consigneeAddressOnSubmit
-      )(dataRequest(request), messages(request)).toString
+        call = consigneeAddressOnSubmit(testErn)
+      )(dataRequest(req, ern = testErn), messages(req)).toString
     }
 
     "must redirect to Journey Recovery for a GET if no existing data is found" in new Fixture(None) {
-      val result = testController.onPageLoad(testErn, testDraftId, NormalMode)(request)
+      val result = testController.onPageLoad(testGreatBritainErn, testDraftId, NormalMode)(request)
 
       status(result) mustEqual SEE_OTHER
       redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
     }
 
     "must redirect to Journey Recovery for a POST if no existing data is found" in new Fixture(None) {
-      val req = FakeRequest(POST, consigneeAddressOnSubmit.url).withFormUrlEncodedBody(("value", "answer"))
+      val req = FakeRequest(POST, consigneeAddressRoute).withFormUrlEncodedBody(("value", "answer"))
 
-      val result = testController.onSubmit(testErn, testDraftId, NormalMode)(req)
+      val result = testController.onSubmit(testGreatBritainErn, testDraftId, NormalMode)(req)
 
       status(result) mustEqual SEE_OTHER
       redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url

a2d569b6a0c8e1b2037dc74c54f89ec4b0940539
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-05-22 14:33:05 +0100

    [ETFE-3910] add redirect to DraftMovementController if emcs-tfe returns 422

--- app/models/response/ErrorResponse.scala
+++ app/models/response/ErrorResponse.scala
@@ -34,6 +34,10 @@ case class UnexpectedDownstreamDraftSubmissionResponseError(status: Int) extends
   override val statusCode: Option[Int] = Some(status)
 }
 
+case object RIMRuleValidationError extends ErrorResponse {
+  val message = "RIM rule validation error"
+}
+
 case object JsonValidationError extends ErrorResponse {
   val message = "JSON validation error"
 }

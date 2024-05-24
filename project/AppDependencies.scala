e83f0289cf7d90ccdc96f708acc19b806e72c44f
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-05-24 16:24:23 +0100

    [ETFE-3910] dependency bump

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 object AppDependencies {
 
   val playSuffix = "-play-30"
-  val hmrcBootstrapVersion = "8.4.0"
-  val hmrcMongoVersion = "1.7.0"
+  val hmrcBootstrapVersion = "8.6.0"
+  val hmrcMongoVersion = "1.9.0"
   val jsoupVersion = "1.17.2"
 
   private val compile = Seq(

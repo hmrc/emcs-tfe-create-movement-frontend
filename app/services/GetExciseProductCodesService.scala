7c61b26b7bb4745417f05985c2629980ba1839c0
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-07-11 14:30:43 +0100

    [ETFE-3426] don't filter EPCs

--- app/services/GetExciseProductCodesService.scala
+++ app/services/GetExciseProductCodesService.scala
@@ -30,29 +30,19 @@ class GetExciseProductCodesService @Inject()(connector: GetExciseProductCodesCon
                                             (implicit ec: ExecutionContext) {
 
 
-  private[services] def filterEPCCodes()(implicit request: DataRequest[_]): PartialFunction[Seq[ExciseProductCode], Seq[ExciseProductCode]] = epcs =>
-    if (request.isUkToUkAndNoGuarantor) {
-      epcs.filter(epc => Set("B000", "W200", "W300")(epc.code))
-    } else if (request.isUnknownDestination) {
-      epcs.filter(_.category.toUpperCase == "E")
-    } else if (request.isUkToEuAndNoGuarantor) {
-      epcs.filter(_.category.toUpperCase == "E")
-    } else {
-      epcs
+  private[services] def removeS600IfDutySuspendedMovement(exciseProductCodes: Seq[ExciseProductCode])
+                                                         (implicit request: DataRequest[_]): Seq[ExciseProductCode] = {
+    //Only include S600 in list if consignor ERN = XIPC/ XIPTA (user is/should be asked for an XIPTA as part of a XIPC flow)
+    request.userTypeFromErn match {
+      case NorthernIrelandTemporaryCertifiedConsignor => exciseProductCodes
+      case _ => exciseProductCodes.filterNot(_.code == "S600")
     }
-
-  private[services] def removeS600IfDutySuspendedMovement()(implicit request: DataRequest[_]): PartialFunction[Seq[ExciseProductCode], Seq[ExciseProductCode]] =
-    epcs =>
-      //Only include S600 in list if consignor ERN = XIPC/ XIPTA (user is/should be asked for an XIPTA as part of a XIPC flow)
-      request.userTypeFromErn match {
-        case NorthernIrelandTemporaryCertifiedConsignor => epcs
-        case _ => epcs.filterNot(_.code == "S600")
-      }
+  }
 
   def getExciseProductCodes()(implicit hc: HeaderCarrier, dataRequest: DataRequest[_]): Future[Seq[ExciseProductCode]] = {
     connector.getExciseProductCodes().map {
       case Left(_) => throw ExciseProductCodesException("No excise product codes retrieved")
-      case Right(exciseProductCodes) => (filterEPCCodes andThen removeS600IfDutySuspendedMovement)(exciseProductCodes)
+      case Right(exciseProductCodes) => removeS600IfDutySuspendedMovement(exciseProductCodes)
     }
   }
 }
--- test/services/GetExciseProductCodesServiceSpec.scala
+++ test/services/GetExciseProductCodesServiceSpec.scala
@@ -21,9 +21,7 @@ import fixtures.ItemFixtures
 import mocks.connectors.MockGetExciseProductCodesConnector
 import models.requests.DataRequest
 import models.response.{ExciseProductCodesException, UnexpectedDownstreamResponseError}
-import models.sections.info.movementScenario.MovementScenario
 import pages.sections.guarantor.GuarantorRequiredPage
-import pages.sections.info.DestinationTypePage
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.HeaderCarrier
@@ -41,7 +39,7 @@ class GetExciseProductCodesServiceSpec extends SpecBase with MockGetExciseProduc
 
     "should return Seq[ExciseProductCode]" - {
 
-      "when Connector returns success from downstream and guarantor is required" in {
+      "when Connector returns success from downstream, trader is not XIPC - exclude S600 in results" in {
 
         implicit val request: DataRequest[_] = dataRequest(FakeRequest(), emptyUserAnswers.set(GuarantorRequiredPage, true))
 
@@ -58,83 +56,6 @@ class GetExciseProductCodesServiceSpec extends SpecBase with MockGetExciseProduc
         val actualResults = testService.getExciseProductCodes().futureValue
 
         actualResults mustBe expectedResult
-      }
-
-      Seq(
-        testNorthernIrelandErn -> MovementScenario.UkTaxWarehouse.NI,
-        testGreatBritainErn -> MovementScenario.UkTaxWarehouse.GB
-      ).foreach { case (userErn, scenario) =>
-        s"when Connector returns success from downstream, trader is an ${userErn.take(2)} trader, guarantor is not required and destinationType is $scenario" in {
-
-          implicit val request: DataRequest[_] = dataRequest(
-            FakeRequest(),
-            ern = testNorthernIrelandErn,
-            answers = emptyUserAnswers
-              .set(GuarantorRequiredPage, false)
-              .set(DestinationTypePage, scenario)
-          )
-
-          val expectedResult = Seq(
-            beerExciseProductCode,
-            wineExciseProductCode,
-            wineExciseProductCode300
-          )
-
-          MockGetExciseProductCodesConnector.getExciseProductCodes().returns(Future(Right(Seq(beerExciseProductCode, wineExciseProductCode, wineExciseProductCode300, spiritExciseProductCode, s600ExciseProductCode, energyExciseProductCode))))
-
-          val actualResults = testService.getExciseProductCodes().futureValue
-
-          actualResults mustBe expectedResult
-        }
-      }
-
-      Seq(
-        MovementScenario.EuTaxWarehouse,
-        MovementScenario.TemporaryRegisteredConsignee,
-        MovementScenario.RegisteredConsignee,
-        MovementScenario.DirectDelivery,
-        MovementScenario.ExemptedOrganisation
-      ).foreach { scenario =>
-        s"when Connector returns success from downstream, trader is an XI trader, guarantor is not required and destinaionType is $scenario" in {
-
-          implicit val request: DataRequest[_] = dataRequest(
-            FakeRequest(),
-            ern = testNorthernIrelandErn,
-            answers = emptyUserAnswers
-              .set(GuarantorRequiredPage, false)
-              .set(DestinationTypePage, scenario)
-          )
-
-          val expectedResult = Seq(
-            energyExciseProductCode
-          )
-
-          MockGetExciseProductCodesConnector.getExciseProductCodes().returns(Future(Right(Seq(beerExciseProductCode, wineExciseProductCode, wineExciseProductCode300, spiritExciseProductCode, s600ExciseProductCode, energyExciseProductCode))))
-
-          val actualResults = testService.getExciseProductCodes().futureValue
-
-          actualResults mustBe expectedResult
-        }
-      }
-
-      s"when Connector returns success from downstream, trader is an XI trader, and destinationType is ${MovementScenario.UnknownDestination}" in {
-        implicit val request: DataRequest[_] = dataRequest(
-          FakeRequest(),
-          ern = testNorthernIrelandErn,
-          answers = emptyUserAnswers
-            .set(DestinationTypePage, MovementScenario.UnknownDestination)
-        )
-
-        val expectedResult = Seq(
-          energyExciseProductCode
-        )
-
-        MockGetExciseProductCodesConnector.getExciseProductCodes().returns(Future(Right(Seq(beerExciseProductCode, wineExciseProductCode, wineExciseProductCode300, spiritExciseProductCode, s600ExciseProductCode, energyExciseProductCode))))
-
-        val actualResults = testService.getExciseProductCodes().futureValue
-
-        actualResults mustBe expectedResult
-
       }
 
       "when Connector returns success from downstream, trader is XIPC - include S600 in results" in {

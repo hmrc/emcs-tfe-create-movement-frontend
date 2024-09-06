0c47509f21883e7c9934188e5e6e1a735bb84974
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2024-09-06 16:06:42 +0100

    [ETFE-4503] Added service to work out if userHasTemplates

---
+++ test/mocks/connectors/MockMovementTemplatesConnector.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mocks.connectors
+
+import connectors.emcsTfe.MovementTemplatesConnector
+import models.requests.DataRequest
+import models.response.ErrorResponse
+import models.response.templates.MovementTemplate
+import org.scalamock.handlers.CallHandler3
+import org.scalamock.scalatest.MockFactory
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait MockMovementTemplatesConnector extends MockFactory {
+
+  val mockMovementTemplatesConnector: MovementTemplatesConnector = mock[MovementTemplatesConnector]
+
+  object MockMovementTemplatesConnector {
+
+    def getList(): CallHandler3[DataRequest[_], HeaderCarrier, ExecutionContext, Future[Either[ErrorResponse, Seq[MovementTemplate]]]] =
+      (mockMovementTemplatesConnector.getList()(_: DataRequest[_], _: HeaderCarrier, _: ExecutionContext)).expects(*, *, *)
+  }
+
+}
